f42a58d8311a06ce9858a10f1575d4547b44bca7
joaovitorcorrea06 <70239873+joaovitorcorrea06@users.noreply.github.com>
2020-09-22 21:30:47 -0300

    exercicio tarefas

---
+++ 01/script.js
@@ -0,0 +1,32 @@
+var botaoAdicionar = document.getElementById ('adicionar');
+var inputTarefa = document.getElementById('tarefa');
+
+inputTarefa.onkeypress = function(evento){
+    if (evento.key === 'enter'){
+        adicionar();
+    }
+};
+
+botaoAdicionar.addEventListener('click', adicionar);
+    
+    function adicionar() {
+            var tarefa = inputTarefa.value;
+            if (tarefa !== ''){
+
+                var li = document.createElement('li');
+                li.innerText = tarefa;
+
+                li.style.color = 'red';
+                li.onclick = function(){
+                    li.style.color= 'green';
+                };
+                li.ondblclick = function(){
+                    li.style.color= 'red';
+                };
+
+                var ol = document.getElementById ('tarefas');
+                ol.appendChild(li);
+
+                inputTarefa.value = "";
+            }
+        }
